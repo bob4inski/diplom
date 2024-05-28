d2758a16fd92ef40485d72b47b7bfbed1c423922
bob4inski <robertstryzhak@yandex.ru>
2024-05-28 09:47:27 +0300

    работает получение пользователей из групп

--- main.py
+++ main.py
@@ -13,8 +13,25 @@ class OpenLDAPClient:
             search_base=search_base, 
             search_filter='(objectClass=posixAccount)', 
             search_scope=SUBTREE, 
-            attributes=['uid', 'userPassword'])
+            attributes=['uid','cn', 'userPassword'])
         return self.conn.entries
+    
+    def get_groups(self, search_base):
+        self.conn.search(
+            search_base=search_base, 
+            search_filter='(objectClass=posixGroup)', 
+            search_scope=SUBTREE,
+            attributes=['cn', 'memberUid'])
+        return self.conn.entries
+    
+    def get_users_in_group(self, group_dn):
+        self.conn.search(
+            search_base=group_dn,
+            search_filter='(objectClass=posixGroup)',
+            search_scope=SUBTREE,
+            attributes=['memberUid'])
+        members = self.conn.entries[0].memberUid if self.conn.entries else []
+        return [member for member in members]
 
 if __name__ == "__main__":
     server_uri = 'ldap://185.241.195.163'
@@ -24,12 +41,19 @@ if __name__ == "__main__":
 
     ldap_client = OpenLDAPClient(server_uri, admin_dn, bind_password)
 
-    print("\nUsers:")
-    users = ldap_client.get_users(search_base)
-    for user in users:
-        print(user.enty_to_ldif())
-    #     print(f"DN: {user['dn']}")
-    #     print(f"  cn: {user['attributes']['cn'][0]}")
-    #     print(f"  uid: {user['attributes']['uid'][0]}")
-    #     print(f"  userPassword: {user['attributes']['userPassword'][0]}")
-    #     print()
+    # print("\nUsers:")
+    # users = ldap_client.get_users(search_base)
+    # for user in users:
+    #     print(user)
+
+    groups = ldap_client.get_groups(search_base)
+    for group in groups:
+        print(group.cn)
+
+    print("\nGroups:")
+    groups = ldap_client.get_groups(search_base)
+    for group in groups:
+        print(f"Group: {group.cn}")
+        member_users = ldap_client.get_users_in_group(group.entry_dn)
+        print(f"Members: {', '.join(member_users)}\n")
+
