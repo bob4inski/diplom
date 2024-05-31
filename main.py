ff6e3feddb09ce06bdbdea5b6b80f0e9578fefec
bob4inski <robertstryzhak@yandex.ru>
2024-05-31 17:08:40 +0300

    обновил класс

--- main.py
+++ main.py
@@ -1,14 +1,56 @@
 from ldap3 import Server, Connection, ALL, SUBTREE, MODIFY_ADD
+import tarantool
 
 class LDAPClient:
-    def __init__(self, server_uri, bind_dn, bind_password, base_dn: str):
+    def __init__(self, server_uri, bind_dn, bind_password, base_dn: str, 
+                 tarantool_host: str,tarantool_port: int):
+        
+        self.tarantool_host = tarantool_host
+        self.tarantool_port = tarantool_port
+
         server = Server(server_uri, get_info=ALL)
         self.base_dn = base_dn
-        self.conn = Connection(server, user=bind_dn, password=bind_password, auto_bind=True)
+
+        Connection(server, user=bind_dn, password=bind_password, auto_bind=True).closed
+       # self.conn.closed
 
     def whoami(self):
         self.conn.extend.standard.who_am_i()
 
+    def check_if_exist_t(self, uid: str):
+        response = self.t_connection.select(space_name='ald', key=uid)
+        if len(response.data) == 0:
+            return False
+        elif len(response.data) == 1:
+            return True
+        
+    def add_user_t(self,space: str, uid: str): 
+        self.t_connection = tarantool.Connection(host=self.tarantool_host,
+                            port=self.tarantool_port
+                            )
+
+        if self.check_if_exist_t(uid):
+            self.t_connection.close()
+            return "user exist"
+        else:
+            user = (uid,)
+            self.t_connection.space(space).insert(user)
+            self.t_connection.close()
+            return "user added"
+        
+
+    def get_users_t(self,space: str):
+        self.t_connection = tarantool.Connection(host=self.tarantool_host,
+                            port=self.tarantool_port
+                            )
+        response = self.t_connection.select(space_name=space)
+        return response.data
+
+    def upload_users_to_tarantool(self,space:str):
+        users = self.get_users(get_all="uid")
+        for user in users:
+            self.add_user_t(space=space,uid=str(user.uid))
+        
     def get_users(self, search_base="",get_all="all"):
         if not search_base:
             search_base = self.base_dn
@@ -16,11 +58,13 @@ class LDAPClient:
             "all":['uid','uidNumber','cn', 'userPassword'],
             "uid":['uid']
         }
+
         self.conn.search(
             search_base=search_base, 
             search_filter='(objectClass=inetOrgPerson)', #inetOrgPerson posixAccount
             search_scope=SUBTREE, 
             attributes=attributes[get_all])
+
         return self.conn.entries
     
     def get_groups(self, search_base=""):
@@ -50,7 +94,6 @@ class LDAPClient:
         attributes: A dictionary of attributes for the user.
         """
         # todo
-
         # name = "amogus"
         # new_user_dn = f"uid={name},ou=users,dc=sirius,dc=com"
         # new_user_attributes = {
@@ -109,21 +152,44 @@ if __name__ == "__main__":
     admin_dn = 'cn=admin,dc=sirius,dc=com'
     base_dn = 'dc=sirius,dc=com'
     bind_password = 'openldap'
+    t_host = '185.241.195.163'
+    t_port = 3301
+    OpenLDAP = LDAPClient(server_uri, admin_dn, bind_password,base_dn=base_dn,
+                          tarantool_host=t_host,tarantool_port=t_port)
 
-    OpenLDAP = LDAPClient(server_uri, admin_dn, bind_password,base_dn=base_dn)
+   # OpenLDAP.upload_users_to_tarantool(space="ald")
+    print("get_users")
+    #a = OpenLDAP.get_users_t(space="ald")
+    a = OpenLDAP.get_users()
+    print(a)
+    # server_uri = 'ldap://192.168.232.80'
+    # admin_dn = 'uid=admin,cn=users,cn=accounts,dc=sirius,dc=com'
+    # base_dn = 'dc=sirius,dc=com'
+    # bind_password = 'openldap'
 
-    server_uri = 'ldap://192.168.232.80'
-    admin_dn = 'uid=admin,cn=users,cn=accounts,dc=sirius,dc=com'
-    base_dn = 'dc=sirius,dc=com'
-    bind_password = 'openldap'
+    # AldPro = LDAPClient(server_uri, admin_dn, bind_password,base_dn=base_dn)
+    
 
-    AldPro = LDAPClient(server_uri, admin_dn, bind_password,base_dn=base_dn)
-    # users = OpenLDAP.get_users(get_all="all") 
+    # user_dn = "uid=new_user,ou=users,dc=sirius,dc=com"
+    # user_attributes = {
+    #     'objectClass': ['inetOrgPerson', 'posixAccount', 'top'],
+    #     'uid': 'new_user',
+    #     'cn': 'New User',
+    #     'sn': 'User',
+    #     'uidNumber': '11011',
+    #     'gidNumber': '11011',
+    #     'homeDirectory': '/home/new_user',
+    #     'userPassword': '{SSHA}PIIGPPpA4gtmxIchvfemwrBgQANEB+Yu',
+    #     'mail': 'new_user@example.com',
+    #     'loginShell': '/bin/bash'
+    # }
+    # OpenLDAP.add_user(user_dn, user_attributes)
+    # # OpenLDAP.add_user(new_user_dn, new_user_attributes)
+    # users = OpenLDAP.get_users(get_all="uid") 
+   
     # #get_all all|uid
     # for user in users:
-    #     print(user)
-
-    # OpenLDAP.add_user(new_user_dn, new_user_attributes)
+        # print(user.uid)
 
     # groups = migrate_from.get_groups()
     # for group in groups:
