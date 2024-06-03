be83701acd5496e669d9a69672681603e1e9b7ea
bob4inski <robertstryzhak@yandex.ru>
2024-06-03 19:04:01 +0300

    10000 users

--- generate_users.py
+++ generate_users.py
@@ -16,15 +16,15 @@ mail: user_{uid}@example.com
 
 # Example user password
 user_password = "{SSHA}H2G95QW4iPRnRauBuzNSpawi2H4pKJB+"
+f = open("users.ldif", "a")
 
 # Generate 10 users starting from UID 30000
-starting_uid_number = 30000
-starting_gid_number = 50000
-for i in range(10):
+starting_uid_number = 41000
+starting_gid_number = 41000
+for i in range(10000):
     # Compute the current user's numeric UID.
     current_uid_number = starting_uid_number + i
     current_gid_number = starting_gid_number + i
     # Substitute into the LDIF template
     user_ldif_entry = ldif_template.format(uid=i, uid_number=current_uid_number, user_password=user_password,gid_number=current_gid_number)
-    print(user_ldif_entry)
-    print()
+    f.write(user_ldif_entry)
